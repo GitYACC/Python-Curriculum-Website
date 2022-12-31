17faf22648f1e462b632dc1711ad6600a44bbe9e
GitYACC <shastrysamarth@gmail.com>
2022-12-31 12:37:41 -0800

    Redid Base layout, started using display type grid

--- pycurr/app/(pages)/python/layout.jsx
+++ pycurr/app/(pages)/python/layout.jsx
@@ -2,29 +2,11 @@
 
 import { useEffect, useState } from 'react';
 
-function FloatingBoard({ children }) {
-    return (
-        <div className="border-lighting">
-            <div className="magic-card">
-                {children}
-            </div>
-        </div>
-    )
-}
-
-function Lighting() {
-    return (
-        <div className="lighting"></div>
-    )
-}
 
 function MagicCard({ children }) {
     return (
-        <div>
-            <Lighting />
-            <FloatingBoard>
-                {children}
-            </FloatingBoard>
+        <div className="magic-card">
+            {children}
         </div>
     )
 }
@@ -34,20 +16,23 @@ function Neumorphic({
     name,
     width = 100,
     height = 100,
-    border_radius = 15
+    border_radius = 15,
+    top = 50,
+    left = 50
 }) {
-    let nmorph = document.getElementsByName(name);
-
     useEffect(() => {
+        let nmorph = document.getElementsByName(name);
         nmorph[0].style.width = `${width}px`;
         nmorph[0].style.height = `${height}px`;
         nmorph[0].style.borderRadius = `${border_radius}px`;
+        nmorph[0].style.top = `${top}%`;
+        nmorph[0].style.left = `${left}%`;
     }, [])
 
     return (
-        <div className="neumorphic" name={name}>
+        <button onClick={() => alert(`${name}`)} className="neumorphic" name={name}>
             {children}
-        </div>
+        </button>
     )
 }
 
@@ -55,9 +40,15 @@ function Neumorphic({
 export default function Dashboard({ children }) {
     return (
         <section>
-            <MagicCard>
-                <Neumorphic name="fbtn" width={300}>Hello</Neumorphic>
-            </MagicCard>
+            {/* <MagicCard> */}
+                {/* <Neumorphic name="fbtn" width={300} left={50}>Hello</Neumorphic> */}
+                {/* <Neumorphic name="fbtn2" width={100} left={10}>World!</Neumorphic> */}
+            {/* </MagicCard> */}
+            <div className="BaseGrid">
+                <div className="SideNav"></div>
+                <div className="Main"></div>
+                <div className="IdeaBoard"></div>
+            </div>
             {children}
         </section>
     )
